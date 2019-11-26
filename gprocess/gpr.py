e0d9a87cd6e224d4559e8b7c3843f3e2af735b14
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2019-11-25 22:47:59 -0500

    Update docstring

--- gprocess/gpr.py
+++ gprocess/gpr.py
@@ -1,5 +1,11 @@
-# GPR model training and prediction
-# Author: Maxim Ziatdinov (email: maxim.ziatdinov@ai4microcopy.com)
+'''
+GPR model training and prediction
+Author: Maxim Ziatdinov (email: maxim.ziatdinov@ai4microcopy.com)
+
+Serves as a high-level wrapper for Gaussian processes module 
+from Pyro probabilistic programming library (https://pyro.ai/)
+for easy work with scientific image (2D) and hyperspectral (3D, 4D) data.
+'''
 
 import time
 import numpy as np
